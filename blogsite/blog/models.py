0dae6e7f3ad078089a7aa62ee92a971507a33e83
Diw <dgupta011101@gmail.com>
2025-05-25 19:43:13 +0530

    added fields and admin site

--- blogsite/blog/models.py
+++ blogsite/blog/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 # Create your models here.
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+    
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
     body = models.TextField()
     publish_at = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True) # date saved automatically on creation
@@ -14,5 +29,6 @@ class Post(models.Model):
     
     class Meta:
         ordering = ['-publish_at']
+        indexes = [models.Index(fields=['-publish_at'])]
         
     
